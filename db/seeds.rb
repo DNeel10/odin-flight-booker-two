aea74ba2406fd528bce4b9509adf5964bdda2dc8
Danny <dneel10@gmail.com>
2024-03-05 00:00:28 -0500

    update airport model with codes and seeded db with airports

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,19 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+codes = %w(BWI IAD PHX LIT LAX SAN 
+           SMF COS DEN BDL FLL MIA 
+           MCO TPA HNL ORD IND FWA 
+           ICT SDF MSY PWM BOS DTW 
+           MSP MCI JAN BIL LNK OMA 
+           MHT LAS ACY EWR ABQ BUF
+           JFK SYR AVL RDU FAR CVG
+           DAY OKC TUL EUG PDX PHL 
+           PIT PVD CHS GSP FSD TRI 
+           MEM BNA HOU DFW SAT SLC 
+           BTV RIC SEA CRW GRB MKE 
+           JAC CYS)
+codes.each do |code|
+  Airport.create(code: code)
+end
